64cdd14e44fce95e9447f0fd690fcb70f54f17be
Frank Hucek <fjh32@case.edu>
2017-02-19 15:16:48 -0500

    state finished

---
+++ state.scm
@@ -0,0 +1,40 @@
+; State definition
+; '(() ())
+; ((x y z) (1 3 5))
+; x->1, y->3
+; lookup x and y
+
+(define newState '(() ()) )
+(define testState '((x y z) (1 3 5)))
+
+; ((x y z) (1 3 5)) -> ((a x y z) (13 1 3 5))
+(define insert
+  (lambda (name value state)
+    (cons (cons name (car state))
+          (cons (cons value (car (cdr state))) '() ) )) )
+
+(define update
+   (lambda (name value state)
+     (insert name value (removeItem name state))))
+
+  
+(define removeItem
+  (lambda (name state)
+    (cond
+      ((null? (car state)) (error "variable not in state"))
+      ((eq? name (caar state)) (cons (cdar state)
+                               (cons (cdr (car (cdr state))) '())))
+      (else (insert (caar state) (caadr state) (removeItem name (cons (cdar state)
+                               (cons (cdr (car (cdr state))) '()))))))))
+
+(define lookup
+  (lambda (name state)
+    (cond
+      ((null? (car state)) (error "variable not in state"))
+      ((eq? name (caar state)) (caadr state))
+      (else (lookup name (cons (cdar state)
+                               (cons (cdr (car (cdr state))) '())))))))
+
+
+
+  
