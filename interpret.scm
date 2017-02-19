983a89ba1e78618fd6124d984579ed0e167354b8
Frank Hucek <fjh32@case.edu>
2017-02-19 16:25:04 -0500

    working on it

--- interpret.scm
+++ interpret.scm
@@ -2,3 +2,24 @@
 (load "simpleParser.scm")
 (load "state.scm")
 
+(define interpret
+  (lambda (filename)
+    (interpret-parse-tree (parser filename) newState) ))
+
+(define interpret-parse-tree
+  (lambda (parsetree state)
+    (cond
+      ((null? parsetree) state)
+      (else (interpret-parse-tree (cdr parsetree) (execute-statement (car parsetree) state))) )))
+
+
+(define operand1
+
+(define execute-statement ; M_statement
+  (lambda (statement state)
+    ((eq? 'var (operator statement)) (execute-declaration statement state))
+    ((eq? '= (operator statement)) (execute-assignment statement state))
+    ((eq? '= (operator statement)) (execute-assignment statement state))
+    ((eq? '= (operator statement)) (execute-assignment statement state))
+    ((eq? '= (operator statement)) (execute-assignment statement state)) ))
+    
