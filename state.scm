b651ed46cb54dfd379ee88a1d6d80d6213ad858e
Joseph Volpe <josephvolpe@case.edu>
2017-03-24 19:29:04 -0400

    abstractions for state

--- state.scm
+++ state.scm
@@ -10,9 +10,14 @@
 (define variables (lambda (state) (if (null? state) null (car state))))
 (define assignedValues (lambda (state) (if (null? (cdr state)) null (car (cdr state)))))
 (define firstVariable (lambda (state) (if (null? (variables state)) null (caar state))))
-(define variablesAfterTheFirst (lambda (state) (if (null? (variables state)) null (cdr (assignedValues state)))))
+(define firstValue (lambda (state) (if (null? (assignedValues state)) null (caadr state))))
+(define variablesAfterTheFirst (lambda (state) (if (null? (variables state)) null (cdr (variables state)))))
 (define valuesAfterTheFirst (lambda (state) (if (null? (assignedValues state)) null (cdr (assignedValues state)))))
-  
+(define addNameToState (lambda (name state) (cons name (variables state))))
+(define addValueToState (lambda (value state) (cons value (assignedValues state))))
+(define addStateLayerAroundValues (lambda (assignedValues) (cons assignedValues '())))
+(define combineVariablesAndValues (lambda (variables assignedValues) (cons variables (addStateLayerAroundValues assignedValues))))
+
 ; ((x y z) (1 3 5)) -> ((a x y z) (13 1 3 5))
 (define updateState
    (lambda (name value state)
@@ -20,30 +25,26 @@
 
 (define insertToState
   (lambda (name value state)
-    (cons (cons name (variables state))
-          (cons (cons value (assignedValues state)) '() ) )) )
+    (combineVariablesAndValues (addNameToState name state) (addValueToState value state))))
 
 (define removeItemFromState
   (lambda (name state)
     (cond
       ((null? (variables state)) (error "variable not in state"))
-      ((eq? name (firstVariable state)) (cons (cdar state)
-                                              (cons (valuesAfterTheFirst state) '())))
-      (else (insertToState (caar state) (caadr state) (removeItemFromState name (cons (cdar state)
-                               (cons (valuesAfterTheFirst state) '()))))))))
+      ((eq? name (firstVariable state)) (combineVariablesAndValues (variablesAfterTheFirst state) (valuesAfterTheFirst state)))
+      (else (insertToState (firstVariable state) (firstValue state)
+                           (removeItemFromState name (combineVariablesAndValues (variablesAfterTheFirst state) (valuesAfterTheFirst state))))))))
 
 (define lookupInState
   (lambda (name state)
     (cond
       ((null? (variables state)) (error "variable not in state"))
-      ((eq? name (caar state)) (caadr state))
-      (else (lookupInState name (cons (cdar state)
-                               (cons (valuesAfterTheFirst state) '())))))))
+      ((eq? name (firstVariable state)) (firstValue state))
+      (else (lookupInState name (combineVariablesAndValues (variablesAfterTheFirst state) (valuesAfterTheFirst state)))))))
 
 (define containsInState
   (lambda (name state)
     (cond
       ((null? (variables state)) #f)
-      ((eq? name (caar state)) #t)
-      (else (containsInState name (cons (cdar state)
-                                 (cons (cdr (cadr state)) '())))))))
+      ((eq? name (firstVariable state)) #t)
+      (else (containsInState name (combineVariablesAndValues (variablesAfterTheFirst state) (valuesAfterTheFirst state)))))))
