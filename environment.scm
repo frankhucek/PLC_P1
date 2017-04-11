85eb564c399bf00b6e7b54d1c9230355bc6770c3
Joseph Volpe <josephvolpe@case.edu>
2017-04-11 19:53:13 -0400

    added method for adding empty stack

--- environment.scm
+++ environment.scm
@@ -4,6 +4,7 @@
 (define testenv (box '( (((a b) (1 2))  ((c d) (3 4)))    (((e f) (5 6))  ((g h) (7 8)))    (((j k) (9 10))) )  ))
 (define newenv (lambda () (box '( ((() ())) )  )))
 (define addStackLayer (lambda (layer env) (begin (set-box! env (cons layer (unbox env))) env)))
+(define pushEmptyStack (lambda (env) (addStackLayer (newStack) env)))
 (define peekTopStack (lambda (env) (car (unbox env))))
 (define popTopStack (lambda (env) (begin (set-box! env (cdr (unbox env))) env)))
 (define globalStack (lambda (env) (globalStackFinder (unbox env))))
