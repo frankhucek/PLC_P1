0360658502aa2962e8167756fb57d03d553278a2
Joseph Volpe <josephvolpe@case.edu>
2017-04-12 20:03:04 -0400

    everythign working but try and catch but updatng env

--- environment.scm
+++ environment.scm
@@ -23,7 +23,16 @@
     (cond
       ((containsInStack name (peek env)) (addStackLayer (updateStack name value (peek env)) (pop env)))
       ((containsInStack name (globalStack env)) (begin (set-box! env (update-global-stack name value (unbox env))) env))
-      (else (error "variable not in the current scope")))))
+      ;(else (error "variable not in the current scope")))))
+
+      (else (begin (set-box! env (update-helper name value (unbox env))) env)) )))
+
+(define update-helper
+  (lambda (name value unboxed-env)
+    (cond
+      ((null? (cdr unboxed-env)) (error "value not in env"))
+      ((containsInStack name (car unboxed-env)) (cons (updateStack name value (car unboxed-env)) (cdr unboxed-env)))
+      (else (cons (car unboxed-env) (update-helper name value (cdr unboxed-env)))) )))
 
 (define update-global-stack
   (lambda (name value env)
@@ -48,8 +57,16 @@
   (lambda (name env)
     (cond
       ((containsInStack name (peek env)) (lookupInStack name (peek env)))
-      (else (lookupInStack name (globalStack env))))))
+      ((containsInGlobal name env) (lookupInStack name (globalStack env)))
+      (else (lookup-helper name (cdr (unbox env)))) )))
 
+(define lookup-helper
+  (lambda (name unboxed-env)
+    (cond
+      ((null? (cdr unboxed-env)) (error "value not in env"))
+      ((containsInStack name (car unboxed-env)) (lookupInStack name (car unboxed-env)))
+      (else (lookup-helper name (cdr unboxed-env))) )))
+                
 ;contains in the top stack/current scope
 (define contains
   (lambda (name env)
